5e30e17d1d85864acd5032a41374bc1d30133b70
kogarasi <kogarasi.cross@gmail.com>
2015-02-04 02:57:50 +0900

    IL135追加

--- source_typescript/weapon.ts
+++ source_typescript/weapon.ts
@@ -6,6 +6,7 @@ module ZWSim {
 		max: Parameter;
 		nexus: Parameter;
 		zw: Parameter;
+		zw_new: Parameter;
 
 		data: any;
 
@@ -14,6 +15,7 @@ module ZWSim {
 			this.max = new Parameter();
 			this.nexus = new Parameter();
 			this.zw = new Parameter();
+			this.zw_new = new Parameter();
 		}
 
 		set( json ){
@@ -28,6 +30,7 @@ module ZWSim {
 			this.now.reset();
 			this.nexus.reset();
 			this.zw.reset();
+			this.zw_new.reset();
 
 			this.data = json;
 		}
@@ -49,6 +52,39 @@ module ZWSim {
 			this.zw.skill = Math.floor( (this.data.zw.skill-this.data.novus.skill) * (this.now.skill/this.max.skill) );
 			this.zw.spell = Math.floor( (this.data.zw.spell-this.data.novus.spell) * (this.now.spell/this.max.spell) );
 			this.zw.pie = Math.floor( (this.data.zw.pie-this.data.novus.pie) * (this.now.pie/this.max.pie) );
+
+			this.zw.acc = Math.floor( (this.data.zw.acc-this.data.novus.acc) * (this.now.acc/this.max.acc) );
+			this.zw.cri = Math.floor( (this.data.zw.cri-this.data.novus.cri) * (this.now.cri/this.max.cri) );
+			this.zw.deter = Math.floor( (this.data.zw.deter-this.data.novus.deter) * (this.now.deter/this.max.deter) );
+			this.zw.parry = Math.floor( (this.data.zw.parry-this.data.novus.parry) * (this.now.parry/this.max.parry) );
+			this.zw.skill = Math.floor( (this.data.zw.skill-this.data.novus.skill) * (this.now.skill/this.max.skill) );
+			this.zw.spell = Math.floor( (this.data.zw.spell-this.data.novus.spell) * (this.now.spell/this.max.spell) );
+			this.zw.pie = Math.floor( (this.data.zw.pie-this.data.novus.pie) * (this.now.pie/this.max.pie) );
+
+			this.zw_new.acc = Math.floor( (this.data.zw_new.acc-this.data.novus.acc) * (this.now.acc/this.max.acc) );
+			this.zw_new.cri = Math.floor( (this.data.zw_new.cri-this.data.novus.cri) * (this.now.cri/this.max.cri) );
+			this.zw_new.deter = Math.floor( (this.data.zw_new.deter-this.data.novus.deter) * (this.now.deter/this.max.deter) );
+			this.zw_new.parry = Math.floor( (this.data.zw_new.parry-this.data.novus.parry) * (this.now.parry/this.max.parry) );
+			this.zw_new.skill = Math.floor( (this.data.zw_new.skill-this.data.novus.skill) * (this.now.skill/this.max.skill) );
+			this.zw_new.spell = Math.floor( (this.data.zw_new.spell-this.data.novus.spell) * (this.now.spell/this.max.spell) );
+			this.zw_new.pie = Math.floor( (this.data.zw_new.pie-this.data.novus.pie) * (this.now.pie/this.max.pie) );
+
+
+			this.zw_new.acc -= this.zw.acc;
+			this.zw_new.cri -= this.zw.cri;
+			this.zw_new.deter -= this.zw.deter;
+			this.zw_new.parry -= this.zw.parry;
+			this.zw_new.skill -= this.zw.skill;
+			this.zw_new.spell -= this.zw.spell;
+			this.zw_new.pie -= this.zw.pie;
+
+			this.zw.acc -= this.nexus.acc;
+			this.zw.cri -= this.nexus.cri;
+			this.zw.deter -= this.nexus.deter;
+			this.zw.parry -= this.nexus.parry;
+			this.zw.skill -= this.nexus.skill;
+			this.zw.spell -= this.nexus.spell;
+			this.zw.pie -= this.nexus.pie;
 		}
 	}
 }
